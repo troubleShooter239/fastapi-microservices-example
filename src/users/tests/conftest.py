944c7ee0945e18238586e63f9348f7d94427d07e
troubleShooter239 <morgun2282@gmail.com>
2024-09-06 08:12:21 +0300

    fix import error

--- src/users/tests/conftest.py
+++ src/users/tests/conftest.py
@@ -5,10 +5,10 @@ from pytest import fixture
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 
 from src.database import get_async_session, Base
-from src.models import User
+# from src.models import User
 from src.main import app
 
-DATABASE_URL_TEST = "sqlite+aiosqlite:///./db.db"
+DATABASE_URL_TEST = "sqlite+aiosqlite:///:memory:"
 engine_test = create_async_engine(DATABASE_URL_TEST, connect_args={"check_same_thread": False})
 async_maker_test = async_sessionmaker(engine_test, autocommit=False, autoflush=False)
 
@@ -35,21 +35,21 @@ async def ac() -> AsyncGenerator[AsyncClient, None]:
         yield ac
 
 
-test_user_1 = User(**{
-    "name": "Alice", "email": "alice@mail.com", "address": "123 Main St", "user_type": "consumer"
-})
-test_user_2 = User(**{
-    "name": "Albert", "email": "albert@mail.com", "address": "127 Main St", "user_type": "supplier"
-})
+# test_user_1 = User(**{
+#     "name": "Alice", "email": "alice@mail.com", "address": "123 Main St", "user_type": "consumer"
+# })
+# test_user_2 = User(**{
+#     "name": "Albert", "email": "albert@mail.com", "address": "127 Main St", "user_type": "supplier"
+# })
 
 
-@fixture(scope='session')
-async def create_test_users():
-    db = await override_get_async_session()
-    db.add(test_user_1)
-    await db.commit()
-    await db.refresh(test_user_1)
+# @fixture(scope='session')
+# async def create_test_users():
+#     db = await override_get_async_session()
+#     db.add(test_user_1)
+#     await db.commit()
+#     await db.refresh(test_user_1)
 
-    db.add(test_user_2)
-    await db.commit()
-    await db.refresh(test_user_2)
+#     db.add(test_user_2)
+#     await db.commit()
+#     await db.refresh(test_user_2)
